7545fffb88e71e4a6bbafff11e30c0e823c0b1bf
timoteoSilveira <timoteo.barros@gmail.com>
2023-09-22 16:33:49 -0300

    feat: solution one

--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -1,9 +1,29 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Numbers {
     class Program {
+
+        static double Pi = 3.14;
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.Write("Entre com o valor do raio: ");
+            double ray = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circuference = GenerateCircumference(ray);
+            double volume = Volume(ray);
+
+            Console.WriteLine("Circuferencia: " + circuference.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+
+        static double GenerateCircumference(double ray) {
+            return 2.0 * Pi * ray;
+        }
+
+        static double Volume(double ray) {
+            return 4.0 / 3.0 * Pi * Math.Pow(ray, 3.0);
         }
     }
 }
