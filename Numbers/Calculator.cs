cabbaee137ced608c1bb6b32f626deb2efcea6a6
timoteoSilveira <timoteo.barros@gmail.com>
2023-09-22 16:39:57 -0300

    feat: second solution

---
+++ Numbers/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers {
+    class Calculator {
+
+        public double Pi = 3.14;
+
+        public double GenerateCircumference(double ray) {
+            return 2.0 * Pi * ray;
+        }
+
+        public double Volume(double ray) {
+            return 4.0 / 3.0 * Pi * Math.Pow(ray, 3.0);
+        }
+
+    }
+}
--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -3,27 +3,19 @@ using System.Globalization;
 
 namespace Numbers {
     class Program {
-
-        static double Pi = 3.14;
         static void Main(string[] args) {
+            Calculator calculator = new Calculator();
+
             Console.Write("Entre com o valor do raio: ");
             double ray = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circuference = GenerateCircumference(ray);
-            double volume = Volume(ray);
+            double circuference = calculator.GenerateCircumference(ray);
+            double volume = calculator.Volume(ray);
 
             Console.WriteLine("Circuferencia: " + circuference.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + calculator.Pi.ToString("F2", CultureInfo.InvariantCulture));
 
-        }
-
-        static double GenerateCircumference(double ray) {
-            return 2.0 * Pi * ray;
-        }
-
-        static double Volume(double ray) {
-            return 4.0 / 3.0 * Pi * Math.Pow(ray, 3.0);
         }
     }
 }
