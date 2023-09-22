c884f9fc82926c563d990ab2b5c9df02391095dc
timoteoSilveira <timoteo.barros@gmail.com>
2023-09-22 17:01:49 -0300

    feat: third way of implementation

--- Numbers/Calculator.cs
+++ Numbers/Calculator.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 namespace Numbers {
     class Calculator {
 
-        public double Pi = 3.14;
+        public static double Pi = 3.14;
 
-        public double GenerateCircumference(double ray) {
+        public static double GenerateCircumference(double ray) {
             return 2.0 * Pi * ray;
         }
 
-        public double Volume(double ray) {
+        public static double Volume(double ray) {
             return 4.0 / 3.0 * Pi * Math.Pow(ray, 3.0);
         }
 
--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -4,17 +4,16 @@ using System.Globalization;
 namespace Numbers {
     class Program {
         static void Main(string[] args) {
-            Calculator calculator = new Calculator();
-
+           
             Console.Write("Entre com o valor do raio: ");
             double ray = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circuference = calculator.GenerateCircumference(ray);
-            double volume = calculator.Volume(ray);
+            double circuference = Calculator.GenerateCircumference(ray);
+            double volume = Calculator.Volume(ray);
 
             Console.WriteLine("Circuferencia: " + circuference.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: " + calculator.Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculator.Pi.ToString("F2", CultureInfo.InvariantCulture));
 
         }
     }
